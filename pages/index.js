0af3f9dedd605922006ee7773ae8de2021936fc3
Joel <74513202+joeltim@users.noreply.github.com>
2021-04-28 15:28:26 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
           A survival Minecraft server for Origins!
         </p>
     
-        <iframe src="https://discord.com/widget?id=799585278557683732&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      <iframe src="https://discord.com/widget?id=799585278557683732&theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </main>
     </div>
   )
